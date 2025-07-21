97cc4def7367d8fb1d9b13575d8a9b0cc70dd480
Ayush Aggarwal <ayushaggarwal42003@gmail.com>
2025-07-21 14:42:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ app.get('/', (req, res) => {
   res.send('Hello from Ayush');
 });
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
   console.log(`App running on port ${port}`);
 });
